9cc0cf89a906b3da7bf1b4d281cbfb0267419930
Muhammad Saddam Nur <saddam.nur@coinbit.id>
2022-02-14 17:33:12 +0700

    Migrate to null safety

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,5 +1,5 @@
 class TwirpException implements Exception {
-  final String message;
+  final String? message;
 
   TwirpException(this.message);
 
@@ -10,15 +10,15 @@ class TwirpException implements Exception {
 }
 
 class TwirpJsonException extends TwirpException {
-  final String code;
-  final String msg;
+  final String? code;
+  final String? msg;
   final dynamic meta;
 
   TwirpJsonException(this.code, this.msg, this.meta) : super(msg);
 
   factory TwirpJsonException.fromJson(Map<String, dynamic> json) {
     return new TwirpJsonException(
-        json['code'] as String, json['msg'] as String, json['meta']);
+        json['code'] as String?, json['msg'] as String?, json['meta']);
   }
 
   @override
